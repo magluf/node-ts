85934e9f23158fa8c8c471c6c31369ac1d281b12
Sergio Magluf <smagluf@gmail.com>
2020-07-27 22:57:57 -0300

    still trying to make heroku not mess with source code on git.

--- util/prod-env.ts
+++ util/prod-env.ts
@@ -1,18 +1,15 @@
 import fs from 'fs';
 
-const dir = process.argv[3] || `dist`;
+const dir = process.argv[2] || `dist`;
 
 const readStream = fs.createReadStream(`package.json`);
 const packageJsonWriteStream = fs.createWriteStream(`./${dir}/package.json`);
 const procfigWriteStream = fs.createWriteStream(`./${dir}/Procfile`);
 
-let prodPackageJson = null;
-
 readStream.on('data', (chunk) => {
   let packageJson = JSON.parse(chunk.toString());
 
-  let { devDependencies, ...rest } = packageJson;
-  prodPackageJson = rest;
+  let { devDependencies, ...prodPackageJson } = packageJson;
 
   packageJsonWriteStream.write(JSON.stringify(prodPackageJson));
   procfigWriteStream.write('web: npm run heroku-start');
